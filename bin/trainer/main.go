579ce0866edda3143ca56755401c907f14f579f5
david <SE.David@protonmail.com>
2022-04-12 17:40:17 +0300

    add svm prediction logic

---
+++ bin/trainer/main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/JustasRimkus/PvP/core"
+	libSvm "github.com/ewalker544/libsvm-go"
+	"github.com/sirupsen/logrus"
+)
+
+func main() {
+	mix, err := readData("../../files/hidenseek")
+	if err != nil {
+		logrus.WithError(err).Fatal("cannot read mix data")
+	}
+
+	output, err := os.OpenFile("entry.train", os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0x644)
+	if err != nil {
+		logrus.WithError(err).Fatal("cannot open output file")
+	}
+
+	visited := make(map[string]struct{})
+	for _, entry := range mix {
+		if _, ok := visited[entry.String()]; ok {
+			continue
+		}
+
+		visited[entry.String()] = struct{}{}
+		if _, err := output.Write([]byte(
+			fmt.Sprintf("%s\n", entry.String()),
+		)); err != nil {
+			logrus.WithError(err).Fatal("writing to output file")
+		}
+	}
+
+	if err := output.Close(); err != nil {
+		logrus.WithError(err).Error("closing output file")
+	}
+
+	param := libSvm.NewParameter()
+	param.KernelType = libSvm.RBF
+	param.CacheSize = 4000
+
+	model := libSvm.NewModel(param)
+
+	problem, err := libSvm.NewProblem("entry.train", param)
+	if err != nil {
+		logrus.WithError(err).Fatal("creating a problem")
+	}
+
+	model.Train(problem)
+	model.Dump("../solver/entry.model")
+}
+
+func readData(file string) ([]core.Entry, error) {
+	data, err := os.ReadFile(file)
+	if err != nil {
+		return nil, err
+	}
+
+	return core.NewFromLines(strings.Split(string(data), "\n")...), nil
+}
--- go.mod
+++ go.mod
@@ -3,9 +3,15 @@ module github.com/JustasRimkus/PvP
 go 1.18
 
 require (
-	github.com/eclipse/paho.mqtt.golang v1.3.5 // indirect
+	github.com/eclipse/paho.mqtt.golang v1.3.5
+	github.com/ewalker544/libsvm-go v0.0.0-20140912030911-ec8445221ede
+	github.com/sirupsen/logrus v1.8.1
+)
+
+require (
+	github.com/alonsovidales/go_matrix v0.0.0-20140305095437-7b3faed43b95 // indirect
+	github.com/alonsovidales/go_ml v0.0.0-20140523095733-ddc0fff76ce2 // indirect
 	github.com/gorilla/websocket v1.4.2 // indirect
-	github.com/sirupsen/logrus v1.8.1 // indirect
 	golang.org/x/net v0.0.0-20200425230154-ff2c4b7c35a0 // indirect
 	golang.org/x/sys v0.0.0-20200323222414-85ca7c5b95cd // indirect
 )
